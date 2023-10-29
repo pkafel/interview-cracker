4defdbbf3f8fdd4d8b8b04291a0ee0792ebe419f
pkafel <3944981+pkafel@users.noreply.github.com>
2023-10-29 22:05:40 +0100

    Update Databricks section to new format

--- src/main/java/com/piotrkafel/databricks/HouseRobber.java
+++ src/main/java/com/piotrkafel/databricks/HouseRobber.java
@@ -5,30 +5,32 @@ import java.util.Arrays;
 public class HouseRobber {
 
     // House Robber part I
+
     public int rob(int[] nums) {
         int[] cache = new int[nums.length];
         Arrays.fill(cache, -1);
-        return rob(nums, cache, nums.length - 1);
+        return robWithMemoization(nums, cache, nums.length - 1);
     }
 
-    // solution with memoization
-    private int rob(int[] nums, int[] resultsCache, int index) {
+
+    // Recursive solution without memoization
+    private int robWithoutMemoization(int[] nums, int index) {
+        if (index < 0) return 0;
+
+        return Math.max(nums[index] + robWithoutMemoization(nums, index - 2), robWithoutMemoization(nums, index - 1));
+    }
+
+    // Recursive solution with memoization
+    private int robWithMemoization(int[] nums, int[] resultsCache, int index) {
         if(index < 0) return 0;
         if(resultsCache[index]!=-1) return resultsCache[index];
 
         int result = Math.max(
-                nums[index] + rob(nums, resultsCache, index - 2),
-                rob(nums, resultsCache, index - 1)
+                nums[index] + robWithMemoization(nums, resultsCache, index - 2),
+                robWithMemoization(nums, resultsCache, index - 1)
         );
         resultsCache[index] = result;
         return result;
-    }
-
-    // solution without memoization
-    private int rob(int[] nums, int index) {
-        if(index < 0) return 0;
-
-        return Math.max(nums[index] + rob(nums, index - 2), rob(nums, index - 1));
     }
 
     // House Robber part II
@@ -40,6 +42,9 @@ public class HouseRobber {
         Arrays.fill(cacheDidntUseLast, -1);
 
         int index=nums.length - 1;
+        // Intuition is that we need to run algorithm on two separated sets of data.
+        // One for case when we robbed last house (and so we cannot rob first one)
+        // and the second case where we didnt rob last house (and soe we can rob first one).
         return Math.max(
                 nums[index] + rob(nums, cacheUsedLast, index - 2, true),
                 rob(nums, cacheDidntUseLast, index - 1, false)
