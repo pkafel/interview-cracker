efdb7a96df6d9e40ab9b71a2efd5b9ad9ac663cc
pkafel <3944981+pkafel@users.noreply.github.com>
2023-11-07 17:47:07 +0100

    Meta target sum of subarray solution

--- src/main/java/com/piotrkafel/meta/SumOfSubarray.kt
+++ src/main/java/com/piotrkafel/meta/SumOfSubarray.kt
@@ -5,6 +5,18 @@ class SumOfSubarray {
     companion object {
 
         fun sumOfSubarray(input: Array<Int>, target: Int): Boolean {
+            var startIndex = 0
+            var currentSum = input[0]
+            for(i in 1 until input.size) {
+                currentSum += input[i]
+
+                while(currentSum > target) {
+                    currentSum -= input[startIndex]
+                    startIndex++
+                }
+
+                if(currentSum == target) return true
+            }
             return false
         }
     }
